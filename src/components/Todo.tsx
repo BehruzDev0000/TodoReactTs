9462689aaed7058c4cece6be686607544bc81fb7
BehruzDev0000 <behruzdev0000@gmail.com>
2026-02-07 20:57:11 +0500

    added a full-screen height for todo-list

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -7,7 +7,7 @@ const Todo = () => {
     const { todos } = useContext(Context)
     
     return (
-    <div className="bg-indigo-500 p-5 rounded-md shadow-md max-w-[800px] mx-auto text-center">
+    <div className="bg-indigo-500 p-5 rounded-md shadow-md max-w-[800px] mx-auto text-center min-h-screen">
             <h2 className="font-bold text-[30px] text-white">Todo List</h2>
       <div className="max-w-[50%] flex flex-col gap-4 mt-5 mx-auto">
         {todos.map((todo: TodoType) => (
